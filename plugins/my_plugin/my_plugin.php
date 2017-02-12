75d7a33b6bd48690f944f479480b6c8f583b92ba
Una LEstrange <ulestrange@campus.itsligo.ie>
2017-02-12 17:46:42 +0000

    added my plugin inital code

---
+++ plugins/my_plugin/my_plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: My_Plugin
+*/
+ 
+
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
