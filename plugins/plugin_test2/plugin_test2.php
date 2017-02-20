9638cf0edd3c1617b95821e9929ead1703647b5c
Una LEstrange <ulestrange@campus.itsligo.ie>
2017-02-20 22:25:18 +0000

    Added attributes to the test2 shortcode plugin

--- plugins/plugin_test2/plugin_test2.php
+++ plugins/plugin_test2/plugin_test2.php
@@ -3,12 +3,56 @@
 Plugin Name: Plugin_Test2
 */
  
-add_shortcode ('itsligoweb', 'test2_itsligoweb');
+add_action ('init', 'test2_shortcodes_init');
+
+function test2_shortcodes_init()
+{
+ add_shortcode ('itsligoweb', 'test2_itsligoweb');
+
+  add_shortcode ('deptweb', 'test2_deptweb');
+
+}
+
+function test2_itsligoweb($atts, $content = null)
+{
+	if ($content == null){
+		return '<a href=https://www.itsligo.ie/about-it-sligo/about-it-sligo/>
+            About IT Sligo </a>';
+	}
+	else{
+		$content = esc_html($content);
+		return "<a href=https://www.itsligo.ie/about-it-sligo/about-it-sligo/>  $content </a>";
+	}
+
+}
+
+
  
- 
-function test2_itsligoweb()
+
+
+function test2_deptweb($atts, $content = null)
 {	
-	return '<a href="http://itsligo.ie"> IT Sligo Home Page </a>';
+   $atts= array_change_key_case((array)$atts, CASE_LOWER);
+	
+	extract (shortcode_atts(array ('staff' => 'none'), $atts) );
+	
+	if ($staff == 'none') {
+	return '<a href="http://itsligo.ie"> General Dept Home Page </a>';
+	}
+	elseif ($staff == 'Una') {
+		return '<a href=""http://itsligo.ie/staff/ulestrange">
+		Una\'s Web Site </a>';
+	}
+	elseif ($staff == 'Alan')
+	{
+		return '<a href=""http://itsligo.ie/staff/akelly"> 
+		Alan\'s Web Site </a>';
+	}
+	else
+	{
+		return "<a href=https://www.itsligo.ie/about-it-sligo/about-it-sligo/>
+	   Default About IT Sligo </a>";
+	}
 }
 
 
